78e458f8fa97567b128b52575c13e5cd8b15ae04
michelleioflai <1213261059@qq.com>
2024-07-20 17:04:16 +0800

    update

---
+++ 00_ALL/00/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "myHead.h"
+
+int main(int argc, char *argv[])
+{
+    std::cout << "=============== begin testing ================" << std::endl;
+
+
+    std::cout << "===============  end testing  ================" << std::endl;
+    return 0;
+}
--- 01_LinearTable/main.cpp
+++ 01_LinearTable/main.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
-#include "include/01_myStack.hpp"
-#include "include/02_queue_link.hpp"
+#include"include/myHead.h"
 
 int main(int argc, char **argv)
 {
 	std::cout << "!!testing begining!! " << std::endl;
 
-	myStack::my_stack();
-	queueLink::my_link();
 
 
 
